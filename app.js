f7e4ea32b55a83a79a1b9ed725f4844001cd98d4
Anton Johansson <anton.xdd@gmail.com>
2017-12-04 10:44:37 +0100

    scraping from dn.se and aftonbladet.se

--- app.js
+++ app.js
@@ -1,7 +1,69 @@
+
 const express = require('express');
 const app = express();
+const scraperjs = require('scraperjs');
 
-app.get('*',(req,res) => res.send('Hello world!'));
+/*
+    SCRAPING
+    SCRAPING
+*/
+
+let newsFromAB = [];
+let newsFromDN = [];
+
+async function scrapeDN(){
+    let ab = scraperjs.StaticScraper.create('https://dn.se');
+    let news = await ab.scrape(($)=>{
+        return $('a[href*="/nyheter"] h2, a[href*="/nyheter"] h3').map(function() {
+            return {
+                text: $(this).text(),
+                url: 'https://dn.se' + $(this).closest('a').attr('href')
+            }
+        }).get();
+    });
+    newsFromDN = news;
+    console.log(news);
+}
+
+async function scrapeAftonbladet(){
+    let ab = scraperjs.StaticScraper.create('https://aftonbladet.se');
+    let news = await ab.scrape(($)=>{
+        return $('a[href*="/nyheter"] h3').map(function() {
+            return {
+                text: $(this).text(),
+                url: 'https://aftonbladet.se' + $(this).closest('a').attr('href')
+            }
+        }).get();
+    });
+    newsFromAB = news;
+    console.log(news);
+}
+
+
+scrapeDN();
+//setInterval(scrapeDN, 60*1000);
+
+scrapeAftonbladet();
+//setInterval(scrapeAftonbladet, 60*1000);
+
+//app.get('*',(req,res) => res.send('Hello world!'));
+app.get('/ab-news',(req,res) => {
+   res.json(newsFromAB);
+});
+
+app.get('/dn-news',(req,res) => {
+   res.json(newsFromDN);
+});
+
+app.get('/all-news', (req, res) => {
+  res.json(
+    newsFromAB.concat(newsFromDN)
+      .sort((a,b) => a.text > b.text ? 1 : -1)
+  )
+});
+
+
+
 
 app.listen(4001,() =>
     console.log('Listening on port 4001')
