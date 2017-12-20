430084276a7b4a05f910ab8eed72bcb0e887401d
Anton Johansson <anton.xdd@gmail.com>
2017-12-20 19:42:55 +0100

    get visitors from /visitors url

--- app.js
+++ app.js
@@ -12,62 +12,6 @@ console.log("credentials", credentials);
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
  
- 
-const dbName = "Vintergatan5a-analystics";
-// Connection URL
-var url = 'mongodb://localhost:27017/Vintergatan5a-analystics';
-
-
-
-
-
-var findDocuments = function(db, callback) {
-      // Get the documents collection
-      var collection = db.collection('visitors');
-      // Find some documents
-      collection.find({}).toArray(function(err, docs) {
-        assert.equal(err, null);
-        console.log("Found the following records");
-        console.dir(docs);
-        callback(docs);
-      });
-}
-
-
-
-
- var insertDocuments = function(db, callback) {
-      // Get the documents collection
-      var collection = db.collection('visitors');
-      // Insert some documents
-      collection.insertMany([{a : 1}], function(err, result) {
-        assert.equal(err, null);
-        console.log("Inserted "+result.ops.length+" documents into the document collection");
-        callback(result);
-      });
-}
-
-
-
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, database) {
-  assert.equal(null, err);
-  console.log("Connected correctly to server");
-    
-    const myDB = database.db(dbName);
-    const collection = myDB.collection('visitors');
-    
-    findDocuments(myDB, function() {
-        console.log("Found record up!");
-    });
-    
-    insertDocuments(myDB, function() {
-        console.log("Inserted record up!");
-    });
-     
-});
-
-
 
 
 /*
@@ -115,10 +59,65 @@ scrapeAftonbladet();
 
 app.use(express.static(__dirname + '/www'));
 
+
+app.get('/visitors',(req,res) => {
+    
+    // Connection URL
+    const dbName = "Vintergatan5a-analystics";
+    var url = 'mongodb://localhost:27017/Vintergatan5a-analystics';
+
+    var findDocuments = function(db, callback) {
+          // Get the documents collection
+          var collection = db.collection('visitors');
+          // Find some documents
+          collection.find({}).toArray(function(err, docs) {
+            assert.equal(err, null);
+            console.log("Found the following records");
+            console.dir(docs);
+            res.json(docs);
+            callback(docs);
+          });
+    }
+
+
+     var insertDocuments = function(db, callback) {
+          // Get the documents collection
+          var collection = db.collection('visitors');
+          // Insert some documents
+          collection.insertMany([{a : 1}], function(err, result) {
+            assert.equal(err, null);
+            console.log("Inserted "+result.ops.length+" documents into the document collection");
+            callback(result);
+          });
+    }
+
+    // Use connect method to connect to the Server
+    MongoClient.connect(url, function(err, database) {
+      assert.equal(null, err);
+      console.log("Connected correctly to server");
+        
+        const myDB = database.db(dbName);
+        const collection = myDB.collection('visitors');
+        
+        findDocuments(myDB, function() {
+            //console.log("Found data!");
+        });
+        
+        //insertDocuments(myDB, function() {
+            //console.log("Inserted data!");
+        //});
+         
+    });
+
+
+   
+});
+
 //app.get('*',(req,res) => res.send('Hello world!'));
 app.get('/ab-news',(req,res) => {
    res.json(newsFromAB);
 });
+
 
 app.get('/dn-news',(req,res) => {
    res.json(newsFromDN);
