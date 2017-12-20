5a0e1f39dca52811476330d390b0efd8fc1c934a
Anton Johansson <anton.xdd@gmail.com>
2017-12-20 19:17:34 +0100

    no message

--- app.js
+++ app.js
@@ -15,6 +15,24 @@ const assert = require('assert');
 // Connection URL
 var url = 'mongodb://localhost:27017/Vintergatan5a-analystics';
 
+
+ var insertDocuments = function(db, callback) {
+      // Get the documents collection
+  var collection = db.collection('visitors');
+  // Insert some documents
+  collection.insertMany([
+    {a : 1}, {a : 2}, {a : 3}
+  ], function(err, result) {
+    assert.equal(err, null);
+    assert.equal(3, result.result.n);
+    assert.equal(3, result.ops.length);
+    console.log("Inserted 3 documents into the document collection");
+    callback(result);
+  });
+}
+
+
+
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
@@ -24,21 +42,10 @@ MongoClient.connect(url, function(err, db) {
     //var documents = database.db.collection('visitors').find({});
      //  console.log("documents",documents);
 
-     var insertDocuments = function(db, callback) {
-      // Get the documents collection
-      var collection = db.collection('visitors');
-      // Insert some documents
-      collection.insertMany([
-        {a : 1}, {a : 2}, {a : 3}
-      ], function(err, result) {
-        assert.equal(err, null);
-        assert.equal(3, result.result.n);
-        assert.equal(3, result.ops.length);
-        console.log("Inserted 3 documents into the document collection");
-        callback(result);
-      });
-    }
-     
+    insertDocuments(db, function() {
+        db.close();
+        console.log("goes here...");
+     });
      
 });
 
